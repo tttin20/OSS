477a02debcaaaaba78f17a190c05505fba423c4d
tttin20 <46011934+tttin20@users.noreply.github.com>
2023-07-07 16:12:41 +0700

    change to angular app

--- Localization/OssResource.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Localization;
-
-namespace Vib.Oss.Localization;
-
-[LocalizationResourceName("Oss")]
-public class OssResource
-{
-    
-}
--- Pages/Index.cshtml.cs
+++
@@ -1,8 +0,0 @@
-﻿using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
-
-namespace Vib.Oss.Pages;
-
-public class IndexModel : AbpPageModel
-{
-    
-}
